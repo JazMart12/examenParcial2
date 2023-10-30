f4cd666131b2d4bea1470f6a235e382a1eb52020
LizbethMorales7 <moraleslizz913@gmail.com>
2023-10-30 10:41:49 -0600

    Cambios

--- ExamenParcial2/Servicios.php
+++ ExamenParcial2/Servicios.php
@@ -64,32 +64,53 @@ if (isset($_SESSION["usuario"])) {
         </div>
 
         <div >
-          <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-              <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
-              <li class="dropdown order-1">
-                  <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
-                  <ul class="dropdown-menu dropdown-menu-right mt-2">
-                     <li class="px-3 py-2">
-                         <form class="form" role="form" action="php/login.php" method="post">
-                              <div class="form-group">
-                                  <input name="usuario" id="emailInput" placeholder="Usuario" class="form-control form-control-sm" type="text" required="">
-                              </div>
-                              <div class="form-group">
-                                  <input name="pass" id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="password" required="">
-                              </div>
-                               
-                              <div class="form-group">
-                                  <button type="submit" class="btn btn-primary btn-block">Login</button>
-                              </div>
-                              <div class="form-group text-center">
-                                  <small><a href="#" data-toggle="modal" data-target="#modalPassword">Registrar cuenta</a></small>
-                              </div>
-                          </form>
-                      </li>
-                  </ul>
-              </li>
-          </ul>
-      </div>
+            <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
+                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
+                <li class="dropdown order-1">
+                    
+                       
+                       <?php
+                    if($sesionActiva){
+                        echo 
+                        '<button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
+                        <ul class="dropdown-menu dropdown-menu-right mt-2">
+                           <li class="px-3 py-2">
+                        <form class="form" role="form" action="php/login.php" method="post">
+                                <div class="form-group">
+                                    <input name="usuario" id="emailInput" placeholder="Usuario" class="form-control form-control-sm" type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input name="pass" id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="password" required="">
+                                </div>
+                                 
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                                </div>
+                                <div class="form-group text-center">
+                            
+                            
+                            <small><a href="#" data-toggle="modal" data-target="#modalPassword">Registrar cuenta</a></small>
+    
+                        </div>
+                            </form>';
+                        } else {
+                            echo '<button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">'.$_SESSION["usuario"].' <span class="caret"></span></button>
+                            <ul class="dropdown-menu dropdown-menu-right mt-2">
+                               <li class="px-3 py-2">
+                            <form class="form" role="form" action="php/login.php" method="post">
+                            <div class="form-group text-center">
+                                    
+                            <small><a href="php/logout.php" id="cerrar_sesion">Cerrar sesion</a></small>
+    
+                        </div>
+                    </form>';
+                        }
+                        ?>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
   </section>
 
   <div id="modalPassword" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
