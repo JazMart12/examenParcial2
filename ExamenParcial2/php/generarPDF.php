65801814397459ac21b24f50859cecb2adb4f469
Carlos <carlosalberto_c.martinez@hotmail.com>
2023-10-30 19:47:32 -0600

    Generar pdf terminado

--- ExamenParcial2/php/generarPDF.php
+++ ExamenParcial2/php/generarPDF.php
@@ -14,118 +14,104 @@
      $ingles = $_SESSION["ingles"];
      $viajar = $_SESSION["viajar"];
      $puesto = $_SESSION["puesto"];
-    class PDF extends FPDF
-{
-// Cabecera de página
-function Header()
-{
-    // Logo
-    $this->Image('image/logo.jpg',10,8,33);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $this->Cell(80);
-    // Título
-    $this->Cell(30,10,'Code Crafters','C');
-    // Salto de línea
-    $this->Ln(20);
-}
+    
 
-// Pie de página
-function Footer()
-{
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
-date_default_timezone_set('America/Mexico_City');
-
-// Obtiene la fecha actual en la zona horaria de México
-$fechaActual = date("Y-m-d H:i:s"); // Formato: Año-Mes-Día Hora:Minuto:Segundo
-// Creación del objeto de la clase heredada
-$pdf = new PDF();
-//$pdf->AliasNbPages();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',14);
-$pdf->Cell(90,25,"Fecha: $fechaActual",0,1);
-$pdf->Cell(40,5,'Datos del Usuario:',0,1);
-$pdf->SetFont('Arial','',12);
-
-$pdf->Cell(40,10,"Nombre: $nombre",0,1);
-$pdf->Cell(40,10,"Apellido Paterno: $apellido1",0,1);
-$pdf->Cell(40,10,"Apellido Materno: $apellido2",0,1);
-$pdf->Cell(40,10,"Fecha de nacimiento:",0,1);
-$pdf->Cell(40,10,"Dia: $dia     Mes: $mes   Anio: $anio",0,1);
-
-$pdf->Cell(40,10,"Correo electronico: $correo",0,1);
-$pdf->Cell(40,10,"Telefono: $telefono",0,1);
-
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40,10,"Habla ingles:",0,1);
-
-$pdf->SetFont('Arial','',12);
-$pdf->Cell(40,10,"$ingles",0,1);
-
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40,10,"Disponible para viajar:",0,1);
-
-$pdf->SetFont('Arial','',12);
-$pdf->Cell(40,10,"$viajar",0,1);
-
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40,10,"Puesto al que aplica:",0,1);
-
-$pdf->SetFont('Arial','',12);
-$pdf->Cell(40,10,"$puesto",0,1);
-
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40,10,"Lenguajes y/o frameworks que maneja:",0,1);
-
-$pdf->SetFont('Arial','',12);
-$pdf->Cell(40,10,"$lenguajesSeleccionados",0,1);
-
-function generarClaveAleatoria($longitud) {
-    $caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-    $clave = '';
-    $caracteresLength = strlen($caracteres);
-
-    for ($i = 0; $i < $longitud; $i++) {
-        $clave .= $caracteres[rand(0, $caracteresLength - 1)];
+     function generarClaveAleatoria($longitud) {
+        $caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+        $clave = '';
+        $caracteresLength = strlen($caracteres);
+    
+        for ($i = 0; $i < $longitud; $i++) {
+            $clave .= $caracteres[rand(0, $caracteresLength - 1)];
+        }
+    
+        return $clave;
     }
-
-    return $clave;
-}
-
-$claveAleatoria = generarClaveAleatoria(8);
+    date_default_timezone_set('America/Mexico_City');
+    $claveAleatoria = generarClaveAleatoria(8);
 $ban = 1;
 $usuario = $_SESSION["usuario"];
 
 $file = fopen("claves.txt","a+");
 $fileContent = file_get_contents("claves.txt");
 
- if (strpos($fileContent, $usuario) !== false)//ya existe un usuario con clave
+ if (strpos($fileContent, $usuario) !== false){//ya existe un usuario con clave
         $ban = 0;
-
-if($ban==0){
-    $claveAleatoria = "Este usuario ya tiene una clave";
-}
-    
+        $claveAleatoria = "El usuario ya tiene una clave";
+ }
+if($ban !== 0){
     fwrite($file, $usuario." ".$claveAleatoria."\r\n");
-    fclose($file);
-$file = fopen("correos.txt","a+");
-fwrite($file,$usuario." ".$correo."\r\n");
-$pdf->Cell(60,55,"Tu clave: $claveAleatoria",0,1);
+    }
+    
+   fclose($file);
+    $file3 = fopen("correos.txt","a+");
+
+    fwrite($file3,$usuario." ".$correo."\r\n");
+    fclose($file3);
+
+// Obtiene la fecha actual en la zona horaria de México
+$fechaActual = date("Y-m-d H:i:s"); // Formato: Año-Mes-Día Hora:Minuto:Segundo
+$pdf = new FPDF();
+$pdf->AddPage();
+
+// Logo
+$pdf->Image('image/logo.jpg', 10, 10, 40);
+// Configura el encabezado del documento
+$pdf->Ln(10);
+$pdf->SetFont('Arial', 'B', 16);
+$pdf->Cell(0, 10, 'Datos del Candidato', 0, 1, 'C');
+
+$pdf->SetFont('Arial', 'B', 14);
+$pdf->Ln(20);
+
+$pdf->SetLineWidth(0.8);
+$pdf->Line(10, $pdf->GetY(), 270, $pdf->GetY());
+$pdf->SetLineWidth(0.2);
+// Agregar la imagen al PDF
+$imagePath = $imagen;
+$x = 10;  // Coordenada X de la imagen
+$y = 70; // Coordenada Y de la imagen
+$width = 50; // Ancho de la imagen en el PDF
+$height = 0; // Altura de la imagen en el PDF (0 para mantener la proporción original)
+
+$pdf->Image($imagen, $x, $y, $width, $height);
+
+
+$date = date('d/m/Y');
+
+$text = "En la fecha, $date, el usuario con datos: $nombre $apellido1 $apellido2 esta solicitandoel puesto de $puesto en nuestra empresa.
+
+Datos generales del solicitante: 
+Telefono: $telefono 
+Fecha de nacimiento: $dia de $mes de $anio.";
+
+$text2 = "Lenguajes y/o frameworks que maneja: $lenguajesSeleccionados. 
+Dispuesto a viajar: $viajar. 
+
+Habla ingles: $ingles.";
+
+$pdf->SetXY(70, 70);
+$pdf->SetFont('Arial', '', 12,  'ISO-8859-1');
+$pdf->MultiCell(0, 10, $text);
+
+$pdf->SetXY(10, 140);
+$pdf->MultiCell(0, 10, $text2);
+$pdf->Cell(0, 10, "Clave generada: {$claveAleatoria}", 0, 1);
 
 
 
-$pdf->Image("image/firma.jpg",20,230,33);
-$pdf->SetFont('Arial','I',10);
-$pdf->Cell(60,55,"Firma del director ejecutivo",0);
-$pdf->Cell(40,15,"Director Carlos C.  Martinez",0);
-$pdf->Image($imagen,20,250,33);
+// Salto de línea
+$pdf->Ln(10);
+
+$logoWidth = 50;
+$pdf->SetY(-45);
+$pdf->SetFont('Arial', '', 12);
+$pdf->Image('image/firma.jpg', 80, 240, $logoWidth);
+$pdf->Cell(0, 10, '____________________________', 0, 1, 'C');
+$pdf->Cell(0, 10, 'Director Carlos C. Contreras', 0, 1, 'C');
+
+// Guarda y muestra el PDF
 $pdf->Output();
+exit;
+
 ?>
